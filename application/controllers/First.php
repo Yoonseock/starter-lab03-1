f3c7a713e5e36e1618ef883c9d21e4920a8b3734
Yoonseock <yyoo7@my.bcit.ca>
2015-09-24 16:24:35 -0700

    Add First Controller

---
+++ application/controllers/First.php
@@ -0,0 +1,24 @@
+<?php
+
+class First extends Application {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    //-------------------------------------------------------------
+    //  The normal pages
+    //-------------------------------------------------------------
+
+    function index() {
+        $this->data['pagebody'] = 'justone';
+        $record = $this->quotes->first();
+        $this->data = array_merge($this->data, $record);
+        
+        $this->render();
+    }
+
+}
+
+/* End of file Welcome.php */
+/* Location: application/controllers/Welcome.php */
